0bbc2a2ec2b4c89afb6b317666eaabb75cb028c5
agelostsal <agelos.tsal@gmail.com>
2022-11-22 11:37:36 +0200

    Add migration that introduces GWDG provider

---
+++ src/main/resources/db/migration/V1.5__add_GWDG_provider.sql
@@ -0,0 +1,8 @@
+-- ------------------------------------------------
+-- Version: v1.5
+--
+-- Description: Migration that introduces the GWDG provider to the available providers
+-- @author: Agelos Tsalapatis
+-- -------------------------------------------------
+
+INSERT INTO provider (id, name) VALUES (4, 'GWDG')
