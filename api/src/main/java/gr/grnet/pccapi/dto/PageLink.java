5b458e709df3ae9b4bb4c1009bf47731b3ad9091
cthermolia-grnet <77612126+cthermolia-grnet@users.noreply.github.com>
2024-03-26 09:38:14 +0200

    pagination (#71)

---
+++ api/src/main/java/gr/grnet/pccapi/dto/PageLink.java
@@ -0,0 +1,24 @@
+package gr.grnet.pccapi.dto;
+
+import lombok.Builder;
+import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
+@Builder
+@Schema(name = "PageLink", description = "An object represents the links of paginated entities.")
+public class PageLink {
+
+  @Schema(
+      type = SchemaType.STRING,
+      implementation = String.class,
+      description = "Uri to paginated entities.",
+      example = "http://localhost:8080/pcc-api/61eeab7bb3b68f5c3f8c4c24/metrics?page=1&size=10")
+  public String href;
+
+  @Schema(
+      type = SchemaType.STRING,
+      implementation = String.class,
+      description = "Descriptor for how the target resource relates to the current resource.",
+      example = "first")
+  public String rel;
+}
--- api/src/main/java/gr/grnet/pccapi/repository/PrefixRepository.java
+++ api/src/main/java/gr/grnet/pccapi/repository/PrefixRepository.java
@@ -1,5 +1,8 @@
 package gr.grnet.pccapi.repository;
 
+import gr.grnet.pccapi.entity.Page;
+import gr.grnet.pccapi.entity.PageQuery;
+import gr.grnet.pccapi.entity.PageQueryImpl;
 import gr.grnet.pccapi.entity.Prefix;
 import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
 import javax.enterprise.context.ApplicationScoped;
@@ -25,5 +28,26 @@ public class PrefixRepository implements PanacheRepositoryBase<Prefix, Integer> 
    */
   public Prefix findByName(String name) {
     return find("name", name).singleResult();
+  }
+
+  /**
+   * Retrieves a page of Prefixes submitted by the specified user.
+   *
+   * @param page The index of the page to retrieve (starting from 0).
+   * @param size The maximum number of assessments to include in a page.
+   * @return A list of Prefixes objects representing the prefixes in the requested page.
+   */
+  public PageQuery<Prefix> fetchPrefixesByPage(int page, int size) {
+
+    var panache = findAll().page(page, size);
+
+    var pageable = new PageQueryImpl<Prefix>();
+    pageable.list = panache.list();
+    pageable.index = page;
+    pageable.size = size;
+    pageable.count = panache.count();
+    pageable.page = Page.of(page, size);
+
+    return pageable;
   }
 }
