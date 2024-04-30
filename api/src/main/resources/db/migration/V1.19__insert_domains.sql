e4443d65154c39b3aec966b2b4c77dd979285302
cthermolia-grnet <77612126+cthermolia-grnet@users.noreply.github.com>
2024-04-30 11:42:29 +0300

    handle domain repo failure by adding the domains through script (#74)

---
+++ api/src/main/resources/db/migration/V1.19__insert_domains.sql
@@ -0,0 +1,16 @@
+-- ------------------------------------------------
+-- Version: v1.19
+-- Insert into domain table
+-- -------------------------------------------------
+INSERT IGNORE INTO domain VALUES
+(1,"scientific_domain-medical_and_health_sciences", "Medical & Health Sciences",
+    "The science of dealing with the maintenance of health and the prevention and treatment of disease."),
+ ( 2, "scientific_domain-agricultural_sciences", "Agricultural Sciences", "Sciences dealing with food and fibre production and processing. They include the technologies of soil cultivation, crop cultivation and harvesting, animal production, and the processing of plant and animal products for human consumption and use." ),
+( 3, "scientific_domain-generic", "Generic", "Generic, Not targetting a specific domain."),
+(4,"scientific_domain-engineering_and_technology", "Engineering & Technology","The application of science and mathematics by which the properties of matter and the sources of energy in nature are made useful to people."),
+( 5,  "scientific_domain-natural_sciences" , "Natural Sciences","Any of the sciences (such as physics, chemistry, or biology) that deal with matter, energy, and their interrelations and transformations or with objectively measurable phenomena." ),
+( 6,"scientific_domain-other","Other", null),
+( 7, "scientific_domain-humanities" , "Humanities","The branches of learning that investigate human constructs and concerns as opposed to natural processes (as in physics or chemistry) and social relations (as in anthropology or economics)."),
+( 8, "scientific_domain-social_sciences" ,   "Social Sciences",  "A branch of science that deals with the institutions and functioning of human society and with the interpersonal relationships of individuals as members of society."   );
+
+
